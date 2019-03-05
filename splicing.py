10a9081020be351aa522c1a814aad0cb15085f38
Trevor Enright <trevor.enright@gmail.com>
2019-03-05 14:36:11 -0800

    using biotools

--- splicing.py
+++ splicing.py
@@ -18,6 +18,8 @@ import time
 import math
 import random
 import matplotlib as plt
+from Bio import Seq
+from itertools import product
 
 #####################
 # Argument Parser   #
@@ -47,6 +49,9 @@ for c in colors:
 	c = c.lstrip('#')
 	rgb_colors.append(tuple(int(c[i:i+2], 16) for i in (0, 2 ,4)))
 
+fancy = {"r":["a","g"],"y":["c","t"],"s":["g","c"],"w":["a","t"],"k":["g","t"],
+	"m":["a","c"],"b":["c","g","t"],"d":["a","g","t"],"h":["a","c","t"],
+	"v":["a","c","g"],"n":["a","c","g","t"], "u":["t"]}
 #############
 # FUNCTIONS #
 #############
@@ -54,7 +59,6 @@ for c in colors:
 def getNextColor(index):
 		#get color palate
 	
-		
 	if index < len(rgb_colors):	
 		return rgb_colors[index]
 	else:
@@ -82,44 +86,29 @@ def find_all(motif_in, sequence_in):
 	N.................any base
 	"""
 	l = []	#list to hold the start positions
-	
+	motif_in = motif_in.upper()
+	motif_in = motif_in.replace("U","T") #deals with RNA motifs
 	#get list of motifs to find in the sequence
-	motif_list = getMotifList([motif_in])
+	motif_list = get_ambiguous(motif_in)
 
 	for motif in motif_list:
 		#find motif in the sequence
-		l.append([m.start() for m in re.finditer(motif,sequence_in.lower())])
+		l.append([m.start() for m in re.finditer(motif,sequence_in.upper())])
 	
 	flatten = [item for sublist in l for item in sublist]
 	
 	return flatten
 
 
-def getMotifList(List):
-	#given a list of motifs, return a list without any
-	#fancy "iupac" characters, by getting ther permutations of each motif
-	fancy = {"r":["a","g"],"y":["c","t"],"s":["g","c"],"w":["a","t"],"k":["g","t"],
-	"m":["a","c"],"b":["c","g","t"],"d":["a","g","t"],"h":["a","c","t"],
-	"v":["a","c","g"],"n":["a","c","g","t"]}
-	
-	#loop through the list of motifs
-	for pos,motif in enumerate(List):
-		#loop through each letter in each motif
-		for position,letter in enumerate(motif):
-			if letter in fancy:
-				#remove the culprite motif from the List
-				#replace it with as many instances of that iupac letter
-				#as is in it's fancy dictionary
-				motif_m = List.pop(pos)
-				motif_m = list(motif_m) #make it mutable
-				for newLetter in fancy[letter]:
-					motif_m[position] = newLetter #change the letter
-					List.append("".join(motif_m))
-				#recurse
-				return getMotifList(List)
-				
-	return List
-
+def get_ambiguous(seq):
+	# taken from "https://stackoverflow.com/questions/27551921/how-to-extend-ambiguous-dna-sequence"
+	# and "https://github.com/biopython/biopython"
+	# and "http://biopython.org/DIST/docs/api/Bio.Alphabet.IUPAC.IUPACAmbiguousDNA-class.html"
+    d = Seq.IUPAC.IUPACData.ambiguous_dna_values
+    ra = []
+    for i in product(*[d[j] for j in seq]):
+        ra.append("".join(i))
+    return ra
 
 def RevComp(nucleotides):
 	"""
@@ -163,6 +152,12 @@ def DrawIt(exons_pos,ex_lengths,motifs_list,gene,fileName):
 	context.move_to(len(gene)+60,height-50)
 	context.show_text("3'")
 	
+	#draw exons
+	for start, lengths in zip(exons_pos,ex_lengths):
+		context.rectangle(int(start[0])+50,height-110,int(lengths),110)
+		context.set_source(pat)
+		context.fill()
+		context.stroke()
 	
 	
 	#draw motifs
@@ -177,7 +172,7 @@ def DrawIt(exons_pos,ex_lengths,motifs_list,gene,fileName):
 		#set color / legend for motif
 		mot = cairo.LinearGradient(0.0,0.0,0.0,0.7)
 		r,g,b = getNextColor(i)
-		mot.add_color_stop_rgba(0,r/255,g/255,b/255,0.9)
+		mot.add_color_stop_rgba(0,r/255,g/255,b/255,0.75)
 		context.rectangle(5,30*j,12,12)
 		context.set_source(mot)
 		context.fill()
@@ -192,12 +187,7 @@ def DrawIt(exons_pos,ex_lengths,motifs_list,gene,fileName):
 
 	
 	
-	#draw exons
-	for start, lengths in zip(exons_pos,ex_lengths):
-		context.rectangle(int(start[0])+50,height-100,int(lengths),100)
-		context.set_source(pat)
-		context.fill()
-		context.stroke()
+	
 
 			
 	surface.finish()
